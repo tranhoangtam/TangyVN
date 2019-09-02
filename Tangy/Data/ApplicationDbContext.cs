241b8695ed65a24525a51ff6ef6a89bd901a83e9
Trần Hoàng Tâm <tranhoangtam@hotmail.com>
2019-09-02 18:47:33 +0700

    Add project files.

---
+++ Tangy/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Models;
+
+namespace Tangy.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Tangy/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Tên")]
+        public string Name { get; set; }
+
+        [Display(Name = "Mô tả")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Thứ tự")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
